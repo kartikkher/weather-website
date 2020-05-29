2f23b540d3d8a6883855a2389d72466d6b2a05e5
Kartik Kher <kartikkher777@gmail.com>
2020-05-29 18:35:27 +0100

    WIP: Project Completed

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,31 @@
+// Init Storage
+const storage = new Storage();
+// Get stored location data
+const weatherLocation = storage.getLocationData();
 // Init Weather object
-const weather = new Weather("Cambridge", "GB");
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 // Init UI
 const ui = new UI();
 
 // Get weather on DOM Load
 document.addEventListener("DOMContentLoaded", getWeather);
+
+// Chnage Location event
+document.getElementById("w-change-btn").addEventListener("click", (e) => {
+  const city = document.getElementById("city").value;
+  const country = document.getElementById("country").value;
+
+  // Change Location
+  weather.changeLocation(city, country);
+
+  // Set Location in LS
+  storage.setLocationData(city, country);
+
+  // get and display weather
+  getWeather();
+  // Close Modal
+  $("#locModal").modal("hide");
+});
 
 function getWeather() {
   weather
